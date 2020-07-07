4841862dbd158e4a8ef2512a108e664af0e2fc31
chigusa <c02110210610204@gmail.com>
2020-07-07 17:13:12 +0800

    Update Readme.md

--- examples/json.go
+++ examples/json.go
@@ -9,15 +9,12 @@ import (
 )
 
 func JSONExample() {
-	resp, err := requests.Post("http://example.com",
-		requests.JSON(map[string]string{"foo": "bar"}),
-		requests.Query("foo", "bar"),
-	)
+	resp, err := requests.Post("http://example.com", requests.JSON(map[string]string{"foo": "bar"}))
 	if err != nil {
 		panic(err)
 	}
 	m := make(map[string]interface{})
-	if err := resp.JSON(m); err != nil {
+	if err := resp.JSON(&m); err != nil {
 		panic(err)
 	}
 	fmt.Println(m)
