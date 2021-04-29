2077d0c15a4ddbaee3e814636f746f78b96cf3da
chigusa <c02110210610204@gmail.com>
2021-04-29 12:57:13 +0800

    adjust NewRequest

--- request.go
+++ request.go
@@ -9,57 +9,7 @@ import (
 
 // NewRequest return a new *http.Request
 func NewRequest(method, url string, opts ...RequestOption) (*http.Request, error) {
-	options := NewOptions()
-	for _, opt := range opts {
-		opt(options)
-		if options.Err != nil {
-			return nil, options.Err
-		}
-	}
-	if options.multipartWriter != nil {
-		if err := options.multipartWriter.Close(); err != nil {
-			return nil, fmt.Errorf("write multipart error %w", err)
-		}
-	}
-
-	ctx := context.Background()
-	if !options.Deadline.IsZero() {
-		ctx, _ = context.WithDeadline(ctx, options.Deadline)
-	}
-	req, err := http.NewRequestWithContext(ctx, method, url, options.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	// set headers
-	for k, v := range options.Headers {
-		switch val := v.(type) {
-		case string:
-			req.Header.Set(k, val)
-		case fmt.Stringer:
-			req.Header.Set(k, val.String())
-		case nil:
-			req.Header.Del(k)
-		default:
-			return nil, fmt.Errorf("value of header [%s] must be string or nil, but %s", k, reflect.TypeOf(v))
-		}
-	}
-
-	// set queries
-	values := req.URL.Query()
-	for k, vs := range options.Queries {
-		for _, v := range vs {
-			values.Add(k, v)
-		}
-	}
-	req.URL.RawQuery = values.Encode()
-
-	// set cookies
-	for k, v := range options.Cookies {
-		req.AddCookie(&http.Cookie{Name: k, Value: v})
-	}
-
-	return req, nil
+	return NewRequestWithContext(context.Background(), method, url, opts...)
 }
 
 // NewRequestWithContext return a new *http.Request
